e7820a8cf3316e13cd4784d7d076ca5f26852186
T Jackson <sweetladyhazle@gmail.com>
2023-02-07 15:32:38 -0600

    Registered Super Model with Admin Site + Completed POST Functionality for Super + Updated Rest Framework Decimal Settings

--- supers/admin.py
+++ supers/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from .models import Super
+
 
 
 
 # Register your models here.
-
+admin.site.register(Super) 
--- supers/views.py
+++ supers/views.py
@@ -1,17 +1,28 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
+
+    if request.method == 'GET':
+       supers = Super.objects.all()
+       serializer = SuperSerializer(supers, many=True)
+       return Response(serializer.data)
 
 
-    serializer = SuperSerializer(supers, many=True)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
 
 
-    return Response(serializer.data)
+
+
 
 
